af7abd37e9cd2c8852e1cd662e38a7058a427f30
Mel Novais Burakowski <mf.novais.98@gmail.com>
2022-11-22 18:44:22 -0300

    fix: color icons fixed

--- src/components/organisms/timeline/Timeline.tsx
+++ src/components/organisms/timeline/Timeline.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import * as S from "./styles";
-import graduation from "../../../assets/graduation.svg"
-import cup from "../../../assets/cup.svg"
-import bank from "../../../assets/bank.svg"
-import farm from "../../../assets/farm.svg"
+import { ReactComponent as FarmLogo } from "../../../assets/farm.svg";
+import { ReactComponent as CupLogo } from "../../../assets/cup.svg";
+import { ReactComponent as GraduationLogo } from "../../../assets/graduation.svg";
+import { ReactComponent as BankLogo } from "../../../assets/bank.svg";
 
 const Timeline: React.FC = () => {
 
@@ -22,14 +22,14 @@ const Timeline: React.FC = () => {
 					<span>
 						<p>2018</p>
 					</span>
-					<img src={cup} alt="cup coffe" />
+					<CupLogo />
 					<p>Desenvolvedora FrontEnd na Software House <a target="_blank" href="https://www.espressolabs.com.br">Espresso Labs</a></p>
 				</S.LineItem>
 				<S.LineItem>
 					<span>
 						<p>2019</p>
 					</span>
-					<img src={graduation} alt="graduation" />
+					<GraduationLogo />
 					<p>Tecnóloga pela  <a target="_blank" href="https://www.fatecmogidascruzes.com.br">Fatec </a> em Análise e Desenvolvimento de Sistemas</p>
 				</S.LineItem>
 
@@ -37,7 +37,7 @@ const Timeline: React.FC = () => {
 					<span>
 						<p>2020</p>
 					</span>
-					<img src={bank} alt="bank" />
+					<BankLogo />
 					<p>Analista de Sistemas no <a target="_blank" href="https://www.itau.com.br/">Itaú Unibanco</a></p>
 				</S.LineItem>
 
@@ -45,7 +45,7 @@ const Timeline: React.FC = () => {
 					<span>
 						<p>2020</p>
 					</span>
-					<img src={farm} alt="farm" />
+					<FarmLogo />
 					<p>Engenheira de Software na <a target="_blank" href="https://www.gaivota.ai/">Gaivota</a></p>
 				</S.LineItem>
 			</S.Line>
