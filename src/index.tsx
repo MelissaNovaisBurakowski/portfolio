8a3a851cd7a1ffc18fbac8a3180528c7e847f686
Mel Novais Burakowski <mf.novais.98@gmail.com>
2022-11-22 18:45:51 -0300

    refactor: css files organized

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './shared/reset.css';
+import './shared/constants.css';
+import './shared/general.css';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
